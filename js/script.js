b8f74577f41ebf0d59a6781e2e0b7cb9fef1187b
rjrom <108962918+rjrom@users.noreply.github.com>
2023-04-05 09:07:13 +0800

    common language constructs

--- js/script.js
+++ js/script.js
@@ -3,3 +3,38 @@ function compare(x,y) {
 }
 
 console.log(compare(11,10))
+
+function a()
+{
+    return
+    {
+        name: "Yaakov"
+    };
+}
+
+function b() {
+    return {
+        name: "Yaakov"
+    };
+}
+
+console.log(a());
+console.log(b());
+
+var sum = 0;
+var i;
+for (i = 0;i < 10; i++) {
+    console.log(i);
+    sum += i;
+}
+console.log(sum);
+//output: 0 - 9
+
+function z() {
+    var a = 2;
+    var b = "2";
+    return {
+        value: a+b
+    };
+}
+console.log(z());
